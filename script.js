469df10b6eb5df7196d6457df15fa8de4af0edf4
saiapoorvareddy <139210028+saiapoorvareddy@users.noreply.github.com>
2024-06-01 04:38:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
